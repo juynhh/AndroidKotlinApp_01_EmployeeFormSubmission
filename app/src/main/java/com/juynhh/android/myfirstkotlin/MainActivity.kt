d195a2523a155085e78d8c7bfcab44f65e0e0ea5
jendy <haduyen0711@gmail.com>
2024-07-09 11:42:01 +0700

    test

--- app/src/main/java/com/juynhh/android/myfirstkotlin/MainActivity.kt
+++ app/src/main/java/com/juynhh/android/myfirstkotlin/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.juynhh.android.myfirstkotlin
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton : Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+        }
+    }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText : TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText : TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val testString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+        //hello just test git
     }
 }
