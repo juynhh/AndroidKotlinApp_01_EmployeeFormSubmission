2c2b2faf65de2ee6f670d0de2e64dd174ba9a34f
jendy <haduyen0711@gmail.com>
2024-07-09 10:38:45 +0700

    modify layout file with TextInputLayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.2.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
